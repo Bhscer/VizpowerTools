bd40c5908cb67a63581b8b71d46f1741f5de1dff
Bhscer <59637756+Bhscer@users.noreply.github.com>
2020-05-17 15:52:20 +0800

    Update main.py
    临时上传
    添加了功能菜单（还没搞定）
    正在推翻之前的方法，用更高效的方法读取文件

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import uuid,re
 import json
 import datetime
 import base64
+from configparser import ConfigParser
 
 app_version = 1.01
 
@@ -125,10 +126,70 @@ print("***************\n"
 main_choice = input("输入你的指令: ")
 
 if main_choice == "3":
+    #信息
+    salt = str(int(time.time()))
+    pwd = string_to_md5(user_name + password + salt + "WINUPON")
+    pwd2 = string_to_md5(user_name + string_to_md5(password) + salt + "WINUPON")
+    device_mac = uuid.uuid1().hex[-12:].upper()
+    device_mac = ":".join(re.findall(r".{2}", device_mac))
+    url_final = "http://" + prefix + ".kehou.com/courseList.action?uid=" + user_name + "&pwd=" + pwd + "&pwd2=" + pwd2 + "&salt=" + salt + "&callfrom=vplogintool&mac=" + device_mac
+    url_final = url_final.replace(" ", "")
+    #print(url_final)
+    r = requests.get(url_final)
+    file_name = "wxb_response.ini"
+    with open(file_name, "wb") as code:
+        code.write(r.content)
 
-    if remembered != 1:
-        print("正在登录")
+    f = open(file_name)
+    r_line2 = f.readlines()[2]
+    f.close
+    # 读取行数
+    line_count = 0
+    f = open(file_name, "r")
+    for line in f.readlines():
+        line_count = line_count + 1
+    f.close
 
+    cfg = ConfigParser()
+    cfg.read(file_name)
+    #print(cfg.sections())
+    #print(cfg.get('userinfo1', 'name'))
+    # 分析
+    if "[error]" in r_line2:
+        f = open(file_name)
+        r_line3 = f.readlines()[3]
+        f = open(file_name)
+        r_line4 = f.readlines()[4]
+        print("发生错误 " + r_line3[6:] + r_line4[8:])
+    elif "[userinfo1]" in r_line2 or "[update]" in r_line2:
+        webuserid = cfg.get('userinfo1', 'webuserid')
+        user_name = cfg.get('userinfo1', 'username')
+        name = cfg.get('userinfo1', 'name')
+        unit = cfg.get('userinfo1', 'unit')
+        class_name = cfg.get('userinfo1', 'classname')
+        role = cfg.get('userinfo1', 'role')
+        f = open(file_name)
+        webuserid = (f.readlines()[3])[10:]
+        webuserid_1 = webuserid.encode("utf-8")
+        s_userid = base64.b64encode(webuserid_1)
+        s_userid = str(s_userid, 'utf-8')
+        #print(s_userid)
+        #s_userid = base64.b64encode(str(webuserid_1))  # base64加密
+        #base64.b64decode("YWFh")  # base64解密
+        s_userid = string_to_md5(s_userid)
+        white_list = requests.get("https://bhscer.github.io/wxb_py/files/white_list.txt")
+        #print(white_list.content.decode(r.encoding))
+        if s_userid in (white_list.content.decode(r.encoding)):
+            s_allowed_flag = "已授权"
+        else:
+            s_allowed_flag = "未授权"
+        print(name.replace("\n","") + " " + role + "\n" + unit +"\n" + class_name +"\n" + user_name + "\n" + "s_id:" + s_userid.replace("\n","") + " （" + s_allowed_flag + "）")
+        input("按任意键退出...")
+    else:
+        print("与服务器联系失败")
+
+elif main_choice == "1":
+    print("正在登录")
     salt = str(int(time.time()))
     pwd = string_to_md5(user_name + password + salt + "WINUPON")
     pwd2 = string_to_md5(user_name + string_to_md5(password) + salt + "WINUPON")
@@ -141,7 +202,6 @@ if main_choice == "3":
     file_name = "wxb_response.txt"
     with open(file_name, "wb") as code:
         code.write(r.content)
-
     f = open(file_name)
     r_line2 = f.readlines()[2]
     f.close
@@ -160,29 +220,7 @@ if main_choice == "3":
         r_line4 = f.readlines()[4]
         print("发生错误 " + r_line3[6:] + r_line4[8:])
     elif "[userinfo1]" in r_line2:
-        #print("您没有正在上的课哦")
-
-        f = open(file_name)
-        user_name = (f.readlines()[4])[9:]
-        f = open(file_name)
-        name = (f.readlines()[5])[5:]
-        f = open(file_name)
-        unit = (f.readlines()[6])[5:]
-        f = open(file_name)
-        class_name = (f.readlines()[7])[10:]
-        f = open(file_name)
-        role = (f.readlines()[8])[5:]
-
-        f = open(file_name)
-        webuserid = (f.readlines()[3])[10:]
-        webuserid_1 = bytes(webuserid, 'utf-8')
-
-        s_userid = base64.b64encode(str(webuserid_1))  # base64加密
-        #base64.b64decode("YWFh")  # base64解密
-
-        r = requests.get(url_final)
-        name_a_role = name.replace("\n","") + " " + role
-        print(name_a_role+ "" + unit +"" + class_name +""  + "" + user_name + "s_id:" + s_userid)
+        print("您没有正在上的课哦")
     elif "[update]" in r_line2:
         print("登陆成功")
         # 保存账号密码etc.
@@ -227,150 +265,134 @@ if main_choice == "3":
             attend_class_number = input("输入您要上的课序号: ")
             if int(attend_class_number) <= class_ing_number:
                 # print(line_count_2)
-                class_info_line_start = 21 + line_count_2 + 59 * (int(attend_class_number) - 1) + 1
-                class_info_line_end = 21 + line_count_2 + 59 * int(attend_class_number)
-                # print(class_info_line_start)
-                # print(class_info_line_end)
-                search_count = class_info_line_start
-                while search_count <= class_info_line_end:
-                    f = open(file_name)
-                    # l_tmp = search_line_count
-                    l_tmp = f.readlines()[search_count]
-                    f.close
-                    # 防止报错
-                    ClassNoteURL = ""
-                    StudentDetailURL = ""
-                    TeacherOverviewURL = ""
-                    TestResultURL = ""
-                    VoteResultURL = ""
-                    search_count = search_count + 1
-                    if "MeetId" in l_tmp:
-                        MeetId = l_tmp[7:]
-                    if "timesId" in l_tmp:
-                        timesId = l_tmp[8:]
-                    if "Meeting-Subject" in l_tmp:
-                        Meeting_Subject = l_tmp[16:]
-                    if "MeetCurrTime" in l_tmp:
-                        MeetCurrTime = l_tmp[13:]
-                    if "Meeting-Duration" in l_tmp:
-                        Meeting_Duration = l_tmp[17:]
-                    if "EastimateTime" in l_tmp:
-                        EastimateTime = l_tmp[14:]
-                    if "MeetStartTime" in l_tmp:
-                        MeetStartTime = l_tmp[14:]
-                    if "Meeting-Chairman" in l_tmp:
-                        Meeting_Chairman = l_tmp[17:]
-                    if "Meeting-Project" in l_tmp:
-                        Meeting_Project = l_tmp[16:]
-                    if "Meeting-BeforeTime" in l_tmp:
-                        Meeting_BeforeTime = l_tmp[19:]
-                    if "userId" in l_tmp:
-                        userId = l_tmp[7:]
-                    if "NickName" in l_tmp:
-                        NickName = l_tmp[9:]
-                    if "Course-Big-PictureUrl" in l_tmp:
-                        Course_Big_PictureUrl = l_tmp[22:]
-                    if "Course-Total-time" in l_tmp:
-                        Course_Total_time = l_tmp[18:]
-                    if "WinAppTitle" in l_tmp:
-                        WinAppTitle = l_tmp[12:]
-                    if "ProjectName" in l_tmp:
-                        ProjectName = l_tmp[12:]
-                    if "NeedSSR" in l_tmp:
-                        NeedSSR = l_tmp[8:]
-                    if "ServerIP" in l_tmp:
-                        ServerIP = l_tmp[9:]
-                    if "Port" in l_tmp:
-                        Port = l_tmp[5:]
-                    if "AutoRecordPrompt" in l_tmp:
-                        AutoRecordPrompt = l_tmp[17:]
-                    if "Meeting-AddTime" in l_tmp:
-                        Meeting_AddTime = l_tmp[16:]
-                    if "ClientType" in l_tmp:
-                        ClientType = l_tmp[11:]
-                    if "ProxyAllocType" in l_tmp:
-                        ProxyAllocType = l_tmp[15:]
-                    if "MultiMeeting" in l_tmp:
-                        MultiMeeting = l_tmp[13:]
-                    if "listenType" in l_tmp:
-                        listenType = l_tmp[11:]
-                    if "MeetingQuitURL" in l_tmp:
-                        MeetingQuitURL = l_tmp[15:]
-                    if "PresidentKey2" in l_tmp:
-                        PresidentKey2 = l_tmp[14:]
-                    if "VerifyKey" in l_tmp:
-                        VerifyKey = l_tmp[14:]
-                    if "signupCount" in l_tmp:
-                        signupCount = l_tmp[12:]
-                    if "SnapUploadURL" in l_tmp:
-                        SnapUploadURL = l_tmp[14:]
-                    if "RestVodURL" in l_tmp:
-                        RestVodURL = l_tmp[11:]
-                    if "QRCodeBaseURL" in l_tmp:
-                        QRCodeBaseURL = l_tmp[14:]
-                    if "ClassNoteURL" in l_tmp:
-                        QRCodeBaseURL = l_tmp[13:]
-                    if "StuSchool" in l_tmp:
-                        StuSchool = l_tmp[10:]
-                    if "StuClass" in l_tmp:
-                        StuClass = l_tmp[9:]
-                    if "StuPhone" in l_tmp:
-                        StuPhone = l_tmp[9:]
-                    if "CameraRemind" in l_tmp:
-                        CameraRemind = l_tmp[13:]
-                    if "CameraSnap" in l_tmp:
-                        CameraSnap = l_tmp[11:]
-                    if "NDConf" in l_tmp:
-                        NDConf = l_tmp[7:]
-                    if "ShowUserCount" in l_tmp:
-                        ShowUserCount = l_tmp[14:]
-                    if "ClassAutoLock" in l_tmp:
-                        ClassAutoLock = l_tmp[14:]
-                    if "IPVCamera" in l_tmp:
-                        IPVCamera = l_tmp[10:]
-                    if "VideoQualityLevel" in l_tmp:
-                        VideoQualityLevel = l_tmp[18:]
-                    if "AllowHLS" in l_tmp:
-                        AllowHLS = l_tmp[9:]
-                    if "NetDiskProtocol" in l_tmp:
-                        NetDiskProtocol = l_tmp[16:]
-                    if "NetDiskUploadURL" in l_tmp:
-                        NetDiskUploadURL = l_tmp[17:]
-                    if "NetDiskNotifyURL" in l_tmp:
-                        NetDiskNotifyURL = l_tmp[17:]
-                    if "NetDiskUserName" in l_tmp:
-                        NetDiskUserName = l_tmp[16:]
-                    if "NetDiskUserPasswd" in l_tmp:
-                        NetDiskUserPasswd = l_tmp[18:]
-                    if "RecordBlackList" in l_tmp:
-                        RecordBlackList = l_tmp[16:]
-                    if "EvaluateURL" in l_tmp:
-                        EvaluateURL = l_tmp[12:]
-                    if "DocURL" in l_tmp:
-                        DocURL = l_tmp[7:]
-                    if "GreenScreenURL" in l_tmp:
-                        GreenScreenURL = l_tmp[15:]
-                    if "FeedbackURL" in l_tmp:
-                        FeedbackURL = l_tmp[12:]
-                    if "MultiVideoChannels" in l_tmp:
-                        MultiVideoChannels = l_tmp[19:]
-                    if "EditTestStdAns" in l_tmp:
-                        EditTestStdAns = l_tmp[15:]
-                    if "STick" in l_tmp:
-                        STick = l_tmp[6:]
-                    if "SKey" in l_tmp:
-                        SKey = l_tmp[5:]
-                    # 可能用不到
-                    if "ClassNoteURL" in l_tmp:
-                        ClassNoteURL = l_tmp[15:]
-                    if "StudentDetailURL" in l_tmp:
-                        StudentDetailURL = l_tmp[6:]
-                    if "TeacherOverviewURL" in l_tmp:
-                        TeacherOverviewURL = l_tmp[5:]
-                    if "TestResultURL" in l_tmp:
-                        TestResultURL = l_tmp[5:]
-                    if "VoteResultURL" in l_tmp:
-                        VoteResultURL = l_tmp[5:]
+                class_enter_name = "[mt" + attend_class_number + "]"
+                cfg = ConfigParser()
+                cfg.read(file_name)
+                MeetId = cfg.get(class_enter_name, 'MeetId')
+                timesId = cfg.get(class_enter_name, 'timesId')
+                if "Meeting-Subject" in l_tmp:
+                    Meeting_Subject = l_tmp[16:]
+                if "MeetCurrTime" in l_tmp:
+                    MeetCurrTime = l_tmp[13:]
+                if "Meeting-Duration" in l_tmp:
+                    Meeting_Duration = l_tmp[17:]
+                if "EastimateTime" in l_tmp:
+                    EastimateTime = l_tmp[14:]
+                if "MeetStartTime" in l_tmp:
+                    MeetStartTime = l_tmp[14:]
+                if "Meeting-Chairman" in l_tmp:
+                    Meeting_Chairman = l_tmp[17:]
+                if "Meeting-Project" in l_tmp:
+                    Meeting_Project = l_tmp[16:]
+                if "Meeting-BeforeTime" in l_tmp:
+                    Meeting_BeforeTime = l_tmp[19:]
+                if "userId" in l_tmp:
+                    userId = l_tmp[7:]
+                if "NickName" in l_tmp:
+                    NickName = l_tmp[9:]
+                if "Course-Big-PictureUrl" in l_tmp:
+                    Course_Big_PictureUrl = l_tmp[22:]
+                if "Course-Total-time" in l_tmp:
+                    Course_Total_time = l_tmp[18:]
+                if "WinAppTitle" in l_tmp:
+                    WinAppTitle = l_tmp[12:]
+                if "ProjectName" in l_tmp:
+                    ProjectName = l_tmp[12:]
+                if "NeedSSR" in l_tmp:
+                    NeedSSR = l_tmp[8:]
+                if "ServerIP" in l_tmp:
+                    ServerIP = l_tmp[9:]
+                if "Port" in l_tmp:
+                    Port = l_tmp[5:]
+                if "AutoRecordPrompt" in l_tmp:
+                    AutoRecordPrompt = l_tmp[17:]
+                if "Meeting-AddTime" in l_tmp:
+                    Meeting_AddTime = l_tmp[16:]
+                if "ClientType" in l_tmp:
+                    ClientType = l_tmp[11:]
+                if "ProxyAllocType" in l_tmp:
+                    ProxyAllocType = l_tmp[15:]
+                if "MultiMeeting" in l_tmp:
+                    MultiMeeting = l_tmp[13:]
+                if "listenType" in l_tmp:
+                    listenType = l_tmp[11:]
+                if "MeetingQuitURL" in l_tmp:
+                    MeetingQuitURL = l_tmp[15:]
+                if "PresidentKey2" in l_tmp:
+                    PresidentKey2 = l_tmp[14:]
+                if "VerifyKey" in l_tmp:
+                    VerifyKey = l_tmp[14:]
+                if "signupCount" in l_tmp:
+                    signupCount = l_tmp[12:]
+                if "SnapUploadURL" in l_tmp:
+                    SnapUploadURL = l_tmp[14:]
+                if "RestVodURL" in l_tmp:
+                    RestVodURL = l_tmp[11:]
+                if "QRCodeBaseURL" in l_tmp:
+                    QRCodeBaseURL = l_tmp[14:]
+                if "ClassNoteURL" in l_tmp:
+                    QRCodeBaseURL = l_tmp[13:]
+                if "StuSchool" in l_tmp:
+                    StuSchool = l_tmp[10:]
+                if "StuClass" in l_tmp:
+                    StuClass = l_tmp[9:]
+                if "StuPhone" in l_tmp:
+                    StuPhone = l_tmp[9:]
+                if "CameraRemind" in l_tmp:
+                    CameraRemind = l_tmp[13:]
+                if "CameraSnap" in l_tmp:
+                    CameraSnap = l_tmp[11:]
+                if "NDConf" in l_tmp:
+                    NDConf = l_tmp[7:]
+                if "ShowUserCount" in l_tmp:
+                    ShowUserCount = l_tmp[14:]
+                if "ClassAutoLock" in l_tmp:
+                    ClassAutoLock = l_tmp[14:]
+                if "IPVCamera" in l_tmp:
+                    IPVCamera = l_tmp[10:]
+                if "VideoQualityLevel" in l_tmp:
+                    VideoQualityLevel = l_tmp[18:]
+                if "AllowHLS" in l_tmp:
+                    AllowHLS = l_tmp[9:]
+                if "NetDiskProtocol" in l_tmp:
+                    NetDiskProtocol = l_tmp[16:]
+                if "NetDiskUploadURL" in l_tmp:
+                    NetDiskUploadURL = l_tmp[17:]
+                if "NetDiskNotifyURL" in l_tmp:
+                    NetDiskNotifyURL = l_tmp[17:]
+                if "NetDiskUserName" in l_tmp:
+                    NetDiskUserName = l_tmp[16:]
+                if "NetDiskUserPasswd" in l_tmp:
+                    NetDiskUserPasswd = l_tmp[18:]
+                if "RecordBlackList" in l_tmp:
+                    RecordBlackList = l_tmp[16:]
+                if "EvaluateURL" in l_tmp:
+                    EvaluateURL = l_tmp[12:]
+                if "DocURL" in l_tmp:
+                    DocURL = l_tmp[7:]
+                if "GreenScreenURL" in l_tmp:
+                    GreenScreenURL = l_tmp[15:]
+                if "FeedbackURL" in l_tmp:
+                    FeedbackURL = l_tmp[12:]
+                if "MultiVideoChannels" in l_tmp:
+                    MultiVideoChannels = l_tmp[19:]
+                if "EditTestStdAns" in l_tmp:
+                    EditTestStdAns = l_tmp[15:]
+                if "STick" in l_tmp:
+                    STick = l_tmp[6:]
+                if "SKey" in l_tmp:
+                    SKey = l_tmp[5:]
+                # 可能用不到
+                if "ClassNoteURL" in l_tmp:
+                    ClassNoteURL = l_tmp[15:]
+                if "StudentDetailURL" in l_tmp:
+                    StudentDetailURL = l_tmp[6:]
+                if "TeacherOverviewURL" in l_tmp:
+                    TeacherOverviewURL = l_tmp[5:]
+                if "TestResultURL" in l_tmp:
+                    TestResultURL = l_tmp[5:]
+                if "VoteResultURL" in l_tmp:
+                    VoteResultURL = l_tmp[5:]
                 print(" ")
                 sif_dontlockclass = input("是否取消自动锁定课堂？ （y/n）")
                 if ("y" in sif_dontlockclass) or ("Y" in sif_dontlockclass):
@@ -522,6 +544,7 @@ if main_choice == "3":
                 print("你输入的内容有误")
     else:
         print("与服务器联系失败")
+
 
 
 
